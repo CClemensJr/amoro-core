3699dbe7bf4d5c76cd39211f0c83d673ff614db2
Charles Clemens <cclemensjr@gmail.com>
2018-11-15 12:11:47 -0800

    Created a player object and set the Player class variables

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -7,6 +7,18 @@ namespace UserInterface
     {
         static void Main(string[] args)
         {
+            //PLAYER OBJECT CONSTRUCTOR
+            // Create an instance of the Player class
+            Player player = new Player();
+
+            // Set the player variables
+            player.CurrentHealthPoints = 10;
+            player.MaxHealthPoints = 10;
+            player.Gold = 0;
+            player.ExperiencePoints = 0;
+            player.Level = 1;
+
+
             Console.WriteLine("Hello World!");
 
             Console.ReadLine();
