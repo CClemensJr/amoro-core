d94e72dec8a1cb83258965dffd2e8dcf148e1be4
Charles Clemens <cclemensjr@gmail.com>
2018-11-15 11:54:39 -0800

    Added Player class to Engine project, declared public vars representing the Players status for rendering in the console.

---
+++ Engine/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Player
+    {
+        // Declare public player variables that will be displayed on the screen.
+        public int CurrentHP { get; set; }
+        public int MaxHP { get; set; }
+        public int Gold { get; set; }
+        public int XP { get; set; }
+        public int Level { get; set; }
+    }
+}
