86ab599440716c77cc72f76d09fe8711da8539f0
Charles Clemens <cclemensjr@gmail.com>
2018-11-15 12:33:44 -0800

    Organized variable declaration in Player class, added PlayerName

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -6,11 +6,12 @@ namespace Engine
 {
     public class Player
     {
-        // Declare public player variables that will be displayed on the screen.
+        // Declare public player attributes that will be displayed on the screen.
         public int CurrentHealthPoints { get; set; }
-        public int MaxHealthPoints { get; set; }
-        public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
+        public int Gold { get; set; }
         public int Level { get; set; }
+        public int MaxHealthPoints { get; set; }
+        public string PlayerName { get; set; }
     }
 }
