e5ab9157e410da15dc3ea9fc1436f7867472eb89
Charles Clemens <cclemensjr@gmail.com>
2018-11-15 14:28:41 -0800

    Declared properties of HealthPotion class

---
+++ Engine/HealthPotion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class HealthPotion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PluralName { get; set; }
+        public int HealAmount { get; set; }
+    }
+}
