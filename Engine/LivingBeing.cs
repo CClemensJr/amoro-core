46416ae265b29a6b66e02ceb3b5138f43695ace3
Charles Clemens <cclemensjr@gmail.com>
2018-11-16 12:40:46 -0800

    Moved ID from Monster class to LivingBeing

--- Engine/LivingBeing.cs
+++ Engine/LivingBeing.cs
@@ -6,6 +6,7 @@ namespace Engine
 {
     public class LivingBeing
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public int CurrentHealthPoints { get; set; }
         public int MaxHealthPoints { get; set; }
--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -6,7 +6,6 @@ namespace Engine
 {
     public class Monster: LivingBeing
     {
-        public int ID { get; set; }
         public int MaxDamage { get; set; }
         public int ExperienceReward { get; set; }
         public int FinancialReward { get; set; }
