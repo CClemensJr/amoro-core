0b651d468c8d7de86fb2509877db948f039319e2
Charles Clemens <cclemensjr@gmail.com>
2018-11-16 12:11:53 -0800

    Removed ID, Name, and PluralName properties from HealthPotion class

--- Engine/HealthPotion.cs
+++ Engine/HealthPotion.cs
@@ -6,9 +6,6 @@ namespace Engine
 {
     public class HealthPotion: Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string PluralName { get; set; }
         public int HealAmount { get; set; }
     }
 }
