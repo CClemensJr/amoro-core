5629629e4be78bb4269ffb53ed8d61cd4b42014d
Charles Clemens <cclemensjr@gmail.com>
2018-11-16 12:38:36 -0800

    Removed inherited properties from Monster

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -7,9 +7,6 @@ namespace Engine
     public class Monster: LivingBeing
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public int MaxHealthPoints { get; set; }
-        public int CurrentHealthPoints { get; set; }
         public int MaxDamage { get; set; }
         public int ExperienceReward { get; set; }
         public int FinancialReward { get; set; }
