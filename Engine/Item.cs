66a71e3f3ede8735e2943292f5fcff4f10050aff
Charles Clemens <cclemensjr@gmail.com>
2018-11-18 19:47:21 -0800

    Added class constructor for Item base class

--- Engine/Item.cs
+++ Engine/Item.cs
@@ -9,5 +9,12 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string PluralName { get; set; }
+
+        public Item(int id, string name, string names)
+        {
+            ID = id;
+            Name = name;
+            PluralName = names;
+        }
     }
 }
