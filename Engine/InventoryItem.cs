486784aefcabf8e520bea38ac74cfd4f4a50dede
Charles Clemens <cclemensjr@gmail.com>
2018-11-20 13:07:42 -0800

    Added details and quantity properties to InventoryItem, created constructor

--- Engine/InventoryItem.cs
+++ Engine/InventoryItem.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Engine
 {
-    class InventoryItem
+    public class InventoryItem
     {
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+
+        public InventoryItem (Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
     }
 }
