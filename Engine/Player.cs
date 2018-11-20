f124250470f4b5937a1b08d9d6c1832591215c01
Charles Clemens <cclemensjr@gmail.com>
2018-11-20 11:08:06 -0800

    Added custom constructor to Player class

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -9,5 +9,12 @@ namespace Engine
         public int ExperiencePoints { get; set; }
         public int Money { get; set; }
         public int Level { get; set; }
+
+        public Player(int id, string name, int currentHP, int maxHP, int xp, int money, int lvl) : base(id, name, currentHP, maxHP)
+        {
+            ExperiencePoints = xp;
+            Money = money;
+            Level = lvl;
+        }
     }
 }
