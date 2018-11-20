28c56fff9d5cb45f42d60db00d0b4792945ec066
Charles Clemens <cclemensjr@gmail.com>
2018-11-20 11:58:37 -0800

    Added ItemReward property to Quest class

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -11,6 +11,7 @@ namespace Engine
         public string Details { get; set; }
         public int ExperienceReward { get; set; }
         public int FinancialReward { get; set; }
+        public Item ItemReward { get; set; }
 
         public Quest(int id, string name, string details, int xp, int money)
         {
