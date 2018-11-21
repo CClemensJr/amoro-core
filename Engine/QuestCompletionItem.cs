bf36c93eda58acedb423873e7994d78cc15fa4d9
Charles Clemens <cclemensjr@gmail.com>
2018-11-21 11:17:04 -0800

    Added properties and constructor to QuestCompletionItem class

--- Engine/QuestCompletionItem.cs
+++ Engine/QuestCompletionItem.cs
@@ -6,6 +6,13 @@ namespace Engine
 {
     public class QuestCompletionItem
     {
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
 
+        public QuestCompletionItem (Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
     }
 }
