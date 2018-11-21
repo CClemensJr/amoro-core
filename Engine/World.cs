d9ae5794c55db794e5ed9506ebe6f9564b248346
Charles Clemens <cclemensjr@gmail.com>
2018-11-21 12:42:24 -0800

    Added static constructor that creates Item, Monster, Quest, and Location objects

--- Engine/World.cs
+++ Engine/World.cs
@@ -44,5 +44,14 @@ namespace Engine
         public const int LOCATION_ID_BRIDGE = 8;
         public const int LOCATION_ID_SPIDER_FIELD = 9;
 
+        
+        static World()
+        {
+            PopulateItems();
+            PopulateMonsters();
+            PopulateQuests();
+            PopulateLocations();
+        }
+
     }
 }
