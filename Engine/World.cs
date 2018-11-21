ace23548585b4a8b640959272d539f84c106de18
Charles Clemens <cclemensjr@gmail.com>
2018-11-21 12:26:12 -0800

    Added List properties that will store all instances of Item, Monster, Quest, and Location

--- Engine/World.cs
+++ Engine/World.cs
@@ -6,5 +6,10 @@ namespace Engine
 {
     public static class World
     {
+        // Lists used to store all game data
+        public static readonly List<Item> Items = new List<Item>();
+        public static readonly List<Monster> Monster = new List<Monster>();
+        public static readonly List<Quest> Quests = new List<Quest>();
+        public static readonly List<Location> Locations = new List<Location>();
     }
 }
