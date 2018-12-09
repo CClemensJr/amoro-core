8f155b4fd851b4277047cf8f82d7ba2c0bbeb2da
CClemensJr <cclemensjr@gmail.com>
2018-12-08 17:14:48 -0800

    Added function for accepting player input

--- UserInterface/Amoro.cs
+++ UserInterface/Amoro.cs
@@ -14,36 +14,47 @@ namespace UserInterface
             // Create an instance of the Amoro class so that Main can make calls to non-static methods
             Amoro a = new Amoro();
 
-            // Set up the console
+            // Create the world of Amoro
+            a.initializeGame();
+
+            // Render in console window
             a.userInterface();
 
             Console.ReadLine();
         }
 
-        public void userInterface()
+
+        //
+        //  GAME SETUP FUNCTIONS
+        //
+        public void initializeGame()
         {
-            createSection("WELCOME TO AMORO", 5, 1);
-            initializeGame();
-            showPlayerDetails();
-            showPlayerInventory();
+            _player = new Player("Leydin", World.LocationByID(World.LOCATION_ID_HOME), 10, 10, 0, 0, 1);
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
         }
 
-        public void createSection(string text, int rows, int spacing)
+
+
+        //
+        // HELPER FUNCTIONS
+        //
+        public void createSection(string text, int rows)
         {
             int screenWidth = Console.WindowWidth;
             int centerText = ((screenWidth) / 2) + ((text.Length) / 2);
-            string border = " ";
+            string borderString = " ";
+            string borderType = "-";
 
             for (int i = 0; i < (screenWidth - 2); i++)
             {
-                border += "*";
+                borderString += borderType;
             }
 
             for (int i = 0; i < rows; i++)
             {
                 if (i == 0 || i == rows - 1)
                 {
-                    Console.WriteLine(String.Format("{0," + (screenWidth - 2) + "}", border));
+                    Console.WriteLine(String.Format("{0," + (screenWidth - 2) + "}", borderString));
                 }
                 else if (i == (rows / 2))
                 {
@@ -51,43 +62,67 @@ namespace UserInterface
                 }
                 else
                 {
-                    Console.WriteLine(String.Format("{0,1} {1," + (screenWidth - 3) + "}", "*", "*"));
+                    Console.WriteLine(String.Format("{0,1} {1," + (screenWidth - 3) + "}", borderType, borderType));
                 }
 
             }
             
         }
 
-        public void initializeGame()
+
+
+        //
+        // USER INTERFACE FUNCTIONS
+        //
+        public void userInterface()
         {
-            _player = new Player("Leydin", World.LocationByID(World.LOCATION_ID_HOME), 10, 10, 0, 0, 1);
-            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
+            createSection("WELCOME TO AMORO", 5);
+            showPlayerDetails();
+            showPlayerInventory();
         }
 
         public void showPlayerDetails()
         {
-            Console.WriteLine( "**************************************************************\n" +
-                              $"**** LOCATION: {_player.CurrentLocation.Name} - {_player.CurrentLocation.Description}                                 ****\n" +
-                               "**************************************************************\n");
+            createSection($"LOCATION: {_player.CurrentLocation.Name} - {_player.CurrentLocation.Description}", 3);
 
-            Console.WriteLine($"LOCATION:   {_player.CurrentLocation.Name}\n" +
+            createSection("", 1);
+            Console.WriteLine($"LOCATION:   {_player.CurrentLocation.Name} - {_player.CurrentLocation.Description}\n" +
                               $"PLAYER:     {_player.Name}\n" +
                               $"LEVEL:      {_player.Level}\n" +
                               $"HP:         {_player.CurrentHealthPoints} / {_player.MaxHealthPoints}\n" +
-                              $"SCHMONEY:   {_player.Money}\n" +
-                              $"XP:         {_player.ExperiencePoints}\n");
+                              $"XP:         {_player.ExperiencePoints}");
+            createSection("", 1);
         }
 
         public void showPlayerInventory()
         {
-            Console.WriteLine("Inventory");
+            createSection("", 1);
+            Console.WriteLine("INVENTORY");
+            Console.WriteLine($"{_player.Money} schmoney");
 
             foreach(InventoryItem item in _player.Inventory)
             {
                 Console.WriteLine(item.Details.Name);
             }
+
+            createSection("", 1);
         }
 
+
+
+        //
+        // USER INPUT
+        //
+        public void acceptPlayerInput(string command)
+        {
+
+        }
+
+
+
+        //
+        // GAMEPLAY FUNCTIONS
+        //
         public void MoveTo(Location newLocation)
         {
             _player.CurrentLocation = newLocation;
